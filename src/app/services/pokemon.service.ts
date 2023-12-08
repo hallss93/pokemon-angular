e0717eceedc1b78134464fe5b1aaf68b31b4e189
Hallison Melo <hallison.pm@gmail.com>
2023-12-08 13:21:40 -0300

    fix: clear fetchs services

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -6,11 +6,7 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
-import { withCache } from '@ngneat/cashew';
-import { Params } from '@angular/router';
 import { environment } from './../../environments/environment';
-import { HttpApiResponse } from '../interfaces/http-api-response.interface';
-import { QueryParams } from '../interfaces/query-params.interface';
 
 const API_URL = environment.serverUrl;
 
@@ -23,40 +19,6 @@ export interface FilterRequest {
 })
 export class PokemonService {
   constructor(private httpClient: HttpClient) {}
-
-  getCards(req: QueryParams): Observable<HttpApiResponse> {
-    const params = new HttpParams({
-      fromString: `${req.q ? 'q=' + req.q : ''}&page=${req.page}&pageSize=${
-        req.pageSize
-      }&orderBy=${req.orderBy}`,
-    });
-
-    return this.httpClient
-      .get<HttpApiResponse>(`${API_URL}/cards`, { params })
-      .pipe(catchError((err) => this.handleError(err)));
-  }
-
-  getListFilters(type: string) {
-    return this.httpClient
-      .get<{ data: string[] }>(`${API_URL}/${type}`, { context: withCache() })
-      .pipe(
-        map((res) => res.data),
-        catchError((err) => this.handleError(err))
-      );
-  }
-
-  getCardsList(params: Params): Observable<HttpApiResponse> {
-    const apiUrl = `${API_URL}/cards`;
-    return this.httpClient
-      .get<HttpApiResponse>(apiUrl, {
-        params: new HttpParams()
-          .set('page', +params['page'])
-          .set('pageSize', params['pageSize'])
-          .set('orderBy', params['orderBy'])
-          .set('q', params['q']),
-      })
-      .pipe(catchError((err) => this.handleError(err)));
-  }
 
   getAll(param?: any): Observable<any[]> {
     let params = new HttpParams();
